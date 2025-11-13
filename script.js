1cf1ff7eae0df1e6cf34f3965b4cdec319abe12e
VortDyn <54004093+VortDyn@users.noreply.github.com>
2025-11-13 22:06:10 +0300

    Upd Images; style

--- script.js
+++ script.js
@@ -20,12 +20,3 @@ document.addEventListener('click', (e) => {
         menuToggle.classList.remove('active');
     }
 });
-
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('.header');
-    if (window.scrollY > 50) {
-        header.style.boxShadow = '0 20px 60px rgba(0, 102, 255, 0.25)';
-    } else {
-        header.style.boxShadow = '0 20px 60px rgba(0, 102, 255, 0.15)';
-    }
-});
